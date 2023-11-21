92869aa903bb55b4200961795276c97277bc0226
Priyanka <priyankathawal20@gmail.com>
2023-11-20 20:18:50 -0500

    Fixed mistakes

---
+++ GarbageCollection.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+/**git
+ * GarbageCollectiongit
+ */
+public class GarbageCollection {
+public int garbageCollection(String[] garbage, int[] travel) 
+{
+        String []  G = {"G","P","GP","GG"};
+        int [] T = {2,4,3};
+        for (int i=0; i<=G.length;i++ )
+        { for (int j=0; j<=T.length; j++)
+
+        {System.out.println("Minimum No of minutes needed to pick up all the garbage:" + i);
+        }
+        }
+      return 0;  
+    }
+}
